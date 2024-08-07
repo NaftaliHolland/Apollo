ed954385f0ce22b0fa44a84f446118695f2c75a7
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2024-08-07 18:39:26 +0300

    Added fee_structure field to class

--- server/apollo/classes/models.py
+++ server/apollo/classes/models.py
@@ -5,6 +5,7 @@ class Class(models.Model):
     name = models.CharField(max_length=50)
     school = models.ForeignKey("schools.School", on_delete=models.CASCADE)
     class_teacher = models.ForeignKey(Teacher, null=True, blank=True, on_delete=models.SET_NULL)
+    fee_structure = models.ForeignKey("fee.FeeStructure", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.name} {self.school.name}"
