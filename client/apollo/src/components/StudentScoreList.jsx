146b61ba8e9f05ae7f7b341f0bc42cd29b8b1dcd
Holland Naftali <naftaliholland01@gmail.com>
2024-10-05 18:59:05 +0300

    Fixed the printing the same student's result card each time by creating a new useRef for each loop iteration for each student(I'll figure out a better way, but this will do for now)

--- client/apollo/src/components/StudentScoreList.jsx
+++ client/apollo/src/components/StudentScoreList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Badge } from "@/components/ui/badge"
 import {
   Table,
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/table"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Input } from "@/components/ui/input"
-import { Search } from 'lucide-react'
+import { Search, FileSpreadsheet, Printer, ChevronUp, ChevronDown } from 'lucide-react'
 import FormDialog from "@/components/FormDialog";
 import AddStudentForm from "@/components/forms/AddStudentForm";
 import { getSubjects, createOrUpdateStudentGrades } from "@/Api/services";
@@ -22,12 +22,6 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label"
 import { useParams } from "react-router-dom";
 import StudentReportForm from "@/components/StudentReportForm";
-import {
-  FileSpreadsheet,
-  Printer,
-  ChevronUp,
-  ChevronDown,
-} from "lucide-react";
 import {
   Card,
   CardContent,
@@ -43,14 +37,13 @@ const NoStudents = () => {
   return (
     <div>
       <h3 className="text-md font-semibold md:text-2xl">No student added to this class yet</h3>
-      <p className="text-lg text-primary">Use the button at the top right to admitt a student</p>
+      <p className="text-lg text-primary">Use the button at the top right to admit a student</p>
     </div>
   )
 }
 
 const StudentScoreList = ({ studentsWithScores, setStudentsWithScores }) => {
-  const componentRef = useRef(null);
-	const { id } = useParams();
+  const { id } = useParams();
   const [search, setSearch] = useState('');
   const [subjects, setSubjects] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -59,29 +52,12 @@ const StudentScoreList = ({ studentsWithScores, setStudentsWithScores }) => {
   const [expandedStudentSubjectScores, setExpandedStudentSubjectScores] = useState(null);
   const [studentSubjectScores, setStudentSubjectScores] = useState([]);
   const { toast } = useToast()
-	const [printClicked, setPrintClicked] = useState(false);
-
-   // Create a function to handle value change
-  // Update studentsWithScores state with new values in the input
-  // Check for change, toggle button inactivity incase there is a change
-  // Only one student's marks can be changed at a time
-  
-  const print = useReactToPrint({
-    contentRef: componentRef,
-  });
-
-  const handlePrint = () => {
-    console.log(printClicked);
-    setPrintClicked(true);
-    console.log(printClicked);
-    print()
-  }
 
   const handleInputChange = (subjectName, value) => {
     setInputChanged(true);
     setStudentSubjectScores(prevState =>
       prevState.map(subjectScore =>
-        subjectScore.subject__name === subjectName? {
+        subjectScore.subject__name === subjectName ? {
           ...subjectScore, score: value
         } : subjectScore
       ))
@@ -92,68 +68,45 @@ const StudentScoreList = ({ studentsWithScores, setStudentsWithScores }) => {
     setStudentSubjectScores(expandedStudent?.subject_scores)
   }, [expandedStudent])
 
-  const updateScore = (studentId, subjectScores) => {
-    setStudentsWithScores(prevState => ({
-      ...prevState,
-      students: prevState.students.map(student => 
-        student.id === studentId
-          ? {
-              ...student,
-              subject_scores: student.subject_scores.map(subject =>
-              subject.subject__name === subjectName
-                ? { ...subject, score: newScore }
-              : subject
-            )
-          }
-        : student
-      )
-    }));
-  };
-  
   const handleSubmit = async () => {
-
     const newSubjectScores = subjects.reduce((acc, item) => {
       const scoreEntry = studentSubjectScores.find(entry => entry.subject__name === item.name);
       acc[item.id] = scoreEntry.score
-      console.log(acc)
       return acc;
     }, {});
 
-    console.log(newSubjectScores);
     setLoading(true);
-    try{
+    try {
       const updatedStudent = {
-      ...expandedStudent,
-      subject_scores: studentSubjectScores};
+        ...expandedStudent,
+        subject_scores: studentSubjectScores
+      };
 
       setExpandedStudent(updatedStudent);
       const response = await createOrUpdateStudentGrades(updatedStudent.id, id, newSubjectScores);
       toast({
-        title: "Scores updated succesfully",
-        description: "Student subjects updated succesfully"
+        title: "Scores updated successfully",
+        description: "Student subjects updated successfully"
       })
-      } catch (error) {
-        console.log(error);
-      
-        toast({
-          title: "Scores could not be updated",
-          description: "Student scores could not be updated",
-          variant: "desctructive",
-          action: <ToastAction altText="Try Again"> Try Again</ToastAction>,
-        })
-      } finally {
-        setLoading(false);
-      }
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Scores could not be updated",
+        description: "Student scores could not be updated",
+        variant: "destructive",
+        action: <ToastAction altText="Try Again">Try Again</ToastAction>,
+      })
+    } finally {
+      setLoading(false);
     }
+  }
 
   useEffect(() => {
     const fetchSubjects = async () => {
       const schoolId = JSON.parse(localStorage.getItem("schoolInfo")).id
       try {
         const response = await getSubjects(schoolId);
-        console.log(response)
         setSubjects(response.data.subjects);
-        console.log(subjects)
       } catch (error) {
         console.log(error);
       }
@@ -162,23 +115,10 @@ const StudentScoreList = ({ studentsWithScores, setStudentsWithScores }) => {
   }, [])
 
   return (
-		<div className="container mx-auto p-4">
+    <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Grading</h1>
       
       <div className="mb-4 flex justify-between items-center">
-    {/*<div className="w-64">
-          <Label htmlFor="class-filter">Filter by Class</Label>
-          <Select onValueChange={setSelectedClass} defaultValue={selectedClass}>
-            <SelectTrigger id="class-filter">
-              <SelectValue placeholder="Select Class" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="All">All Classes</SelectItem>
-              <SelectItem value="10A">Class 10A</SelectItem>
-              <SelectItem value="10B">Class 10B</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>*/}
         <Button>
           <FileSpreadsheet className="w-4 h-4 mr-2" />
           Generate Merit List
@@ -191,62 +131,69 @@ const StudentScoreList = ({ studentsWithScores, setStudentsWithScores }) => {
         </CardHeader>
         <CardContent>
           <ScrollArea className="h-[calc(100vh-300px)]">
-            {studentsWithScores.students.map(student => (
-              <div key={student.id} className="mb-6 border-b pb-6">
-                <div className="flex justify-between items-center">
-                  <div>
-                    <h3 className="text-lg font-temibold">{student.first_name + " " + student.last_name}</h3>
-                    <p className="text-sm text-gray-500">Class: {student._class.name}</p>
-                  </div>
+            {studentsWithScores.students.map(student => {
+              const studentRef = useRef(null);
+              const handlePrint = useReactToPrint({
+                contentRef: studentRef,
+              });
 
-                  <div style={{ display: "none" }}>
-                     <StudentReportForm student={student} ref={componentRef} />
-                  </div>
+              return (
+                <div key={student.id} className="mb-6 border-b pb-6">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <h3 className="text-lg font-semibold">{student.first_name + " " + student.last_name}</h3>
+                      <p className="text-sm text-gray-500">Class: {student._class.name}</p>
+                    </div>
 
-                  <div className="flex gap-2">
-                    <Button
-                      size="sm"
-                      onClick={() => handlePrint() }>
-                      <Printer className="w-4 h-4 mr-2" />
-                      Print Report
-                    </Button>
-                    <Button 
-                      onClick={() => setExpandedStudent(expandedStudent?.id === student.id ? null : student)} 
-                      size="sm" 
-                      variant="outline"
-                    >
-                      {expandedStudent?.id === student.id ? <ChevronUp className="w-4 h-4" /> : <ChevronDown className="w-4 h-4" />}
-                    </Button>
-                  </div>
-                </div>
-                {expandedStudent?.id === student.id && (
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-                    {studentSubjectScores?.map(subject => (
-                      <div key={subject.subject__name} className="flex flex-col items-center">
-                        <Label htmlFor={`${student.id}-${subject.subject__name}`} className="mb-1 text-center">
-                          {subject.subject__name}
-                        </Label>
-                        <Input
-                          id={`${student.id}-${subject.subject__name}`}
-                          type="number"
-                          min="0"
-                          max="100"
-                          value={subject.score}
-                          onChange={ (e) => handleInputChange(subject.subject__name, e.target.value) }
-                          className="w-16 text-center"
-                        />
-                      </div>
-                    ))}
-                    <Button
-                      onClick = { () => handleSubmit() }
-                      disabled = { !inputChanged }
+										<div style={{display: 'none'}}>
+                      <StudentReportForm student={student} ref={studentRef} />
+                    </div>
+
+                    <div className="flex gap-2">
+                      <Button
+                        size="sm"
+                        onClick={handlePrint}>
+                        <Printer className="w-4 h-4 mr-2" />
+                        Print Report
+                      </Button>
+                      <Button 
+                        onClick={() => setExpandedStudent(expandedStudent?.id === student.id ? null : student)} 
+                        size="sm" 
+                        variant="outline"
                       >
-                      { loading? "... Loading" : "Save Changes" }
-                    </Button>
+                        {expandedStudent?.id === student.id ? <ChevronUp className="w-4 h-4" /> : <ChevronDown className="w-4 h-4" />}
+                      </Button>
+                    </div>
                   </div>
-                )}
-              </div>
-            ))}
+                  {expandedStudent?.id === student.id && (
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+                      {studentSubjectScores?.map(subject => (
+                        <div key={subject.subject__name} className="flex flex-col items-center">
+                          <Label htmlFor={`${student.id}-${subject.subject__name}`} className="mb-1 text-center">
+                            {subject.subject__name}
+                          </Label>
+                          <Input
+                            id={`${student.id}-${subject.subject__name}`}
+                            type="number"
+                            min="0"
+                            max="100"
+                            value={subject.score}
+                            onChange={(e) => handleInputChange(subject.subject__name, e.target.value)}
+                            className="w-16 text-center"
+                          />
+                        </div>
+                      ))}
+                      <Button
+                        onClick={() => handleSubmit()}
+                        disabled={!inputChanged}
+                      >
+                        {loading ? "... Loading" : "Save Changes"}
+                      </Button>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
           </ScrollArea>
         </CardContent>
       </Card>
